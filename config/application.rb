42b7cf6c04de58ac0e971e8491f89e3de67f9a00
Ivan Linko <linko.ivan@gmail.com>
2012-05-10 22:33:45 +0300

    changes in app.rb

--- config/application.rb
+++ config/application.rb
@@ -55,5 +55,7 @@ module FoodTracker
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.generators.stylesheet_engine = :scss
   end
 end
