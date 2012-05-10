226abaee521dba320d1ba98de4de343af4bec9b9
Ivan Linko <linko.ivan@gmail.com>
2012-05-10 21:32:06 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FoodTracker::Application.config.secret_token = '0ee1d8aea26b9addc39f22f555963dccd00e14938fd49e6cecab7532e2540fcb8d609d4c2cc2540ed2490bc8a86e48e826506e0c2192f6d8f21bbc5777cb38a7'
