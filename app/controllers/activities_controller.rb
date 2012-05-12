07dd969574fb108927ae737783a1941b576d5694
Ivan Linko <linko.ivan@gmail.com>
2012-05-13 02:29:51 +0300

    implement multi activities form

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -6,21 +6,40 @@ class ActivitiesController < ApplicationController
   respond_to :html, :json
 
   def create
-    params[:activity][:user_id] = current_user.id
+    @activities = []
+    descriptions = params[:activity][:descriptions]
+    params[:activity].delete(:descriptions)
 
-    @activity = Activity.new params[:activity]
+    descriptions .each do |description|
+      activity             = Activity.new(params[:activity])
+      activity.user        = current_user
+      activity.description = description
 
-    if @activity.save
-      respond_to do |format|
-        format.html { redirect_to collection_path }
-        format.json { render json: @activity , status: :created }
-      end
-    else
-      respond_to do |format|
-        format.html { render :edit }
-        format.json { render json: @activity.errors, status: :unprocessable_entity  }
+      if activity.save
+        @activities << activity
       end
     end
+
+    respond_to do |format|
+      format.html { redirect_to collection_path }
+      format.json { render json: @activities , status: :created }
+    end
+
+    #params[:activity][:user_id] = current_user.id
+    #
+    #@activity = Activity.new params[:activity]
+    #
+    #if @activity.save
+    #  respond_to do |format|
+    #    format.html { redirect_to collection_path }
+    #    format.json { render json: @activity , status: :created }
+    #  end
+    #else
+    #  respond_to do |format|
+    #    format.html { render :edit }
+    #    format.json { render json: @activity.errors, status: :unprocessable_entity  }
+    #  end
+    #end
 
   end
 
