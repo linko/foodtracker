6241e6e7038908e38f4f6c8b0229c10ecaf09c49
Ivan Linko <linko.ivan@gmail.com>
2012-05-12 19:35:32 +0300

    improove json response

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -7,16 +7,36 @@ class ActivitiesController < ApplicationController
 
   def create
     params[:activity][:user_id] = current_user.id
-    create! do |format|
-      format.html { redirect_to collection_path }
-      format.json { respond_with resource }
+
+    @activity = Activity.new params[:activity]
+
+    if @activity.save
+      respond_to do |format|
+        format.html { redirect_to collection_path }
+        format.json { render json: @activity , status: :created }
+      end
+    else
+      respond_to do |format|
+        format.html { render :edit }
+        format.json { render json: @activity.errors, status: :unprocessable_entity  }
+      end
     end
+
   end
 
   def update
-    update! do |format|
-      format.html { redirect_to collection_path }
-      format.json { respond_with resource }
+    @activity.update_attributes params[:activity]
+
+    if @activity.save
+      respond_to do |format|
+        format.html { redirect_to collection_path }
+        format.json { render json: @activity , status: :created }
+      end
+    else
+      respond_to do |format|
+        format.html { render :edit }
+        format.json { render json: @activity.errors, status: :unprocessable_entity  }
+      end
     end
   end
 
