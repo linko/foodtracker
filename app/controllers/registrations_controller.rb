28f4d7a9313722c8f2887f38af691fc250cb670d
Ivan Linko <linko.ivan@gmail.com>
2012-05-14 22:49:08 +0300

    experimenting with CORS

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,20 +1,20 @@
 class RegistrationsController < Devise::RegistrationsController
-  respond_to :json, :html
-  skip_before_filter :verify_authenticity_token, :only => :create
-
-  def create
-    respond_to do |format|
-      format.html {
-        super
-      }
-      format.json {
-        build_resource
-        if resource.save
-          render :status => 200, :json => resource
-        else
-          render :json => resource.errors, :status => :unprocessable_entity
-        end
-      }
-    end
-  end
+  #respond_to :json, :html
+  #skip_before_filter :verify_authenticity_token, :only => :create
+  #
+  #def create
+  #  respond_to do |format|
+  #    format.html {
+  #      super
+  #    }
+  #    format.json {
+  #      build_resource
+  #      if resource.save
+  #        render :status => 200, :json => resource
+  #      else
+  #        render :json => resource.errors, :status => :unprocessable_entity
+  #      end
+  #    }
+  #  end
+  #end
 end
