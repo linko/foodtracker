28785a01859b3289adefefa9fedb1f2a6385ecf8
Ivan Linko <linko.ivan@gmail.com>
2012-05-14 21:42:23 +0300

    remove before filter in devise controller

--- app/controllers/registrations_controller.rb
+++
@@ -1,4 +0,0 @@
-class RegistrationsController < Devise::RegistrationsController
-  skip_before_filter :verify_authenticity_token
-  respond_to :html, :json
-end
--- app/controllers/sessions_controller.rb
+++
@@ -1,4 +0,0 @@
-class SessionsController < Devise::SessionsController
-  skip_before_filter :verify_authenticity_token
-  respond_to :html, :json
-end
