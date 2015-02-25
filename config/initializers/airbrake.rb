ae88939067279020c913e38e4ca8f74093ca0909
Dmitriy Baklikov <dmitriy@railsmuffin.com>
2015-02-25 17:27:35 +0300

    Reverted airbrake back to hndsm

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,3 +1,6 @@
 Airbrake.configure do |config|
-  config.api_key = 'f6085602c2fe3be5728d4abc8b6b4acc'
+  config.api_key = '076c9f345d71448eaae86738e7f35d43'
+  config.host    = 'errbit.hnd.sm.ua'
+  config.port    = 80
+  config.secure  = config.port == 443
 end
